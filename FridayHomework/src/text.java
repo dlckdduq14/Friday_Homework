60b4422d7b37ed840189f377f750b42cef9762f3
SuChang Hwang <hsc8930@gmail.com>
2019-04-18 15:03:03 +0900

    test

--- FridayHomework/src/text.java
+++ FridayHomework/src/text.java
@@ -1,6 +1,7 @@
 
 public class text {
-public static void main(String[] args) {
-	System.out.println("안녕하세요");
-}
+	public static void main(String[] args) {
+		System.out.println("�븞�뀞�븯�꽭�슂");
+		System.out.println("eclipes");
+	}
 }
