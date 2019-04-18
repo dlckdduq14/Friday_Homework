12bb1e5d40bb42667f3892e6fbf4cb1b99a1cc85
soundmusic <wisu69@naver.com>
2019-04-18 15:13:26 +0900

    제대로 고침

--- FridayHomework/src/text.java
+++ FridayHomework/src/text.java
@@ -1,11 +1,10 @@
 
 public class text {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 
 		System.out.println("하이요");
-=======
->>>>>>> refs/remotes/origin/master
+
 		System.out.println("eclipes");
 		System.out.println("안녕하세요");
 
