a28e3fab3cfb6b4e9359b6c95d4bb532a37fd636
soundmusic <wisu69@naver.com>
2019-04-18 15:11:31 +0900

    하이요로 바꿈

--- FridayHomework/src/text.java
+++ FridayHomework/src/text.java
@@ -2,7 +2,7 @@
 public class text {
 	public static void main(String[] args) {
 
-		System.out.println("�븞�뀞�븯�꽭�슂");
+		System.out.println("하이요");
 		System.out.println("eclipes");
 
 		System.out.println("안녕하세요");
